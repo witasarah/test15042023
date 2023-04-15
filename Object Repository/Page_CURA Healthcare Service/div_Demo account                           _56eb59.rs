00ea894e9ca34973451d0df8e1b47f70f41275d2
witas <witas@Witasarah>
2023-04-15 23:53:11 +0700

    commit kedua

---
+++ Object Repository/Page_CURA Healthcare Service/div_Demo account                           _56eb59.rs
@@ -0,0 +1,208 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Demo account                           _56eb59</name>
+   <tag></tag>
+   <elementGuidId>cbec6f76-cc40-4e7c-8304-e779ec4007fb</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//section[@id='login']/div/div/div[2]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div.col-sm-offset-3.col-sm-6</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>3c0cf392-bd25-40b8-8960-b2c7e38ab45d</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>col-sm-offset-3 col-sm-6</value>
+      <webElementGuid>b11b2f40-66eb-4d39-be80-2cd667ad3ea3</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+                
+                    
+                        
+                            Demo account
+                            
+                                
+                                  
+                                  
+                                
+                            
+                        
+                        
+                            
+                                
+                                  
+                                  
+                                
+                            
+                        
+                    
+                    
+                        Username
+                        
+                            
+                        
+                    
+                    
+                        Password
+                        
+                            
+                        
+                    
+                    
+                        
+                            Login
+                        
+                    
+                
+            </value>
+      <webElementGuid>11306fdd-9ee0-4e26-b196-354f8a2abe51</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;login&quot;)/div[@class=&quot;container&quot;]/div[@class=&quot;row&quot;]/div[@class=&quot;col-sm-offset-3 col-sm-6&quot;]</value>
+      <webElementGuid>98675c50-9041-4292-b766-488503266dd9</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//section[@id='login']/div/div/div[2]</value>
+      <webElementGuid>d88d8cf3-3e5c-4523-a752-fdd9bd9d8818</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Login'])[2]/following::div[1]</value>
+      <webElementGuid>d8ce3baf-8571-4bd8-8429-6c529982ea60</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Make Appointment'])[1]/following::div[4]</value>
+      <webElementGuid>a9931605-b68b-43ef-8968-9156989aa2ca</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]</value>
+      <webElementGuid>7082bf4e-f1a2-4041-be18-db97519b7a71</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = '
+                
+                    
+                        
+                            Demo account
+                            
+                                
+                                  
+                                  
+                                
+                            
+                        
+                        
+                            
+                                
+                                  
+                                  
+                                
+                            
+                        
+                    
+                    
+                        Username
+                        
+                            
+                        
+                    
+                    
+                        Password
+                        
+                            
+                        
+                    
+                    
+                        
+                            Login
+                        
+                    
+                
+            ' or . = '
+                
+                    
+                        
+                            Demo account
+                            
+                                
+                                  
+                                  
+                                
+                            
+                        
+                        
+                            
+                                
+                                  
+                                  
+                                
+                            
+                        
+                    
+                    
+                        Username
+                        
+                            
+                        
+                    
+                    
+                        Password
+                        
+                            
+                        
+                    
+                    
+                        
+                            Login
+                        
+                    
+                
+            ')]</value>
+      <webElementGuid>afe60349-4ddd-4319-815e-da5f1e950e7e</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Object Repository/Page_CURA Healthcare Service/i_CURA Healthcare_fa fa-bars.rs
@@ -0,0 +1,59 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>i_CURA Healthcare_fa fa-bars</name>
+   <tag></tag>
+   <elementGuidId>9265eda8-8082-414f-a9a0-891e8bb2a75b</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[@id='menu-toggle']/i</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>i.fa.fa-bars</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>i</value>
+      <webElementGuid>c2727525-f708-4067-a2c3-aefe048f2b5d</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>fa fa-bars</value>
+      <webElementGuid>e1aae829-652f-44bc-8f5b-c1944733a6b3</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;menu-toggle&quot;)/i[@class=&quot;fa fa-bars&quot;]</value>
+      <webElementGuid>db131bcc-1623-4b10-962c-6ef58fc4b7c1</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//a[@id='menu-toggle']/i</value>
+      <webElementGuid>0b35dd82-4324-4cc0-8d54-71ff378bfa31</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//i</value>
+      <webElementGuid>7181c050-70c0-4059-a21a-42a2fd305158</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
